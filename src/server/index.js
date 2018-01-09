3d531b1dfac2ce527bb8f118067c82251d319317
Adam <kontakt@adamszajuk.pl>
2018-01-09 15:38:32 +0100

    Project setup (#1)

---
+++ src/server/index.js
@@ -0,0 +1,30 @@
+import path from 'path'
+import dotenv from 'dotenv'
+import Express from 'express'
+
+import IndexController from './controllers/IndexController'
+import ModulesController from './controllers/ModulesController'
+
+// use .env variables
+dotenv.config()
+
+const app = new Express()
+const port = process.env.PORT || 3000
+
+// Use PUG as template engine
+app.set('view engine', 'pug')
+
+// Use src/server/views as templates root folder
+app.set('views', path.join(__dirname, 'views'))
+
+// Use build as static files root folder
+app.use(Express.static(path.join(__dirname, '../../build')))
+
+// Routes
+app.all('/', IndexController)
+app.all('/modules', ModulesController)
+
+// Fire up server
+app.listen(port, function () {
+  console.log(`App server is running on port ${port}`)
+})
